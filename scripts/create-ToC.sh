5877a0ff6d8e41015dbb8807fe9a12b77aa49354
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-02-18 17:41:31 +0100

    patch

--- scripts/create-ToC.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-focuspath=~"/Library/Mobile Documents/iCloud~md~obsidian/Documents/Main Vault/.obsidian/themes/Shimmering Focus.css"
-grep -E "<+ " "$focuspath" | sed -e "s/ \*\///" -e "s/\/\* //" -e "s/<<< /\t\t- /" -e "s/<< /\t- /" -e "s/< /- /" | tail -n +2 | pbcopy
-osascript -e 'display notification "" with title "ToC copied."'
