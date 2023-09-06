c68e3ed601f2733b2940b291ed468199bf28706a
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-09-06 14:20:25 +0200

    publish (automated)

--- build.sh
+++ build.sh
@@ -116,7 +116,7 @@ sed -E -i '' "s/badge.*-[[:digit:]]+-/badge\/downloads-$dl-/" ./README.md
 # add to git
 git add -A && git commit -m "publish (automated)"
 git pull 
-# git push
+git push
 
 #───────────────────────────────────────────────────────────────────────────────
 # INFO specific to my setup: copy theme file for fallback
