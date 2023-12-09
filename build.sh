b5eff58d328248b74a0e8dddc5fd7893a6dcd8e5
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2023-12-09 13:05:50 +0100

    build: simply by removing linting & ToC creation

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
-#!/usr/bin/env zsh
-#───────────────────────────────────────────────────────────────────────────────
-
 # WHAT THIS SCRIPT DOES
 # - Check the yaml from the Style Settings for errors. If there are any, the
 #   build is aborted, the errors are passed (e.g. for a notification) and the
 #   Style Settings tab is opened.
-# - takes argument as commit message (uses "chore" if executed without argument)
 # - bumps version number in css file
-# - The docs are linted and checked for invalid links
-# - ToC in the CSS file is updated
 # - updates download counts in badges of the .md files
 # - updates the stylelint-file
 # - git add, commit, pull, and push to the remote repo
@@ -21,18 +15,13 @@ CSS_PATH="$script_dir/theme.css"
 
 #───────────────────────────────────────────────────────────────────────────────
 
-# DEPENDENCIES
-npm_location="$(npm root)/.bin/"
-export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$npm_location:$PATH
-
-if ! command -v stylelint &>/dev/null; then echo "stylelint not installed." && return 1; fi
-if ! command -v yaml-validator &>/dev/null; then echo "yaml-validator not installed." && return 1; fi
-
-#───────────────────────────────────────────────────────────────────────────────
-
 # YAML VALIDATION
 # - Abort build if yaml invalid
 # - requires style settings placed at the very bottom of the theme css
+npm_location="$(npm root)/.bin/"
+export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$npm_location:$PATH
+if ! command -v yaml-validator &>/dev/null; then echo "yaml-validator not installed." && return 1; fi
+
 sed -n '/@settings/,$p' "$CSS_PATH" | sed '1d;$d' | sed '$d' >temp.yml
 YAMLLINT_OUTPUT=$(yamllint-validator temp.yml)
 if [[ $? == 1 ]]; then
@@ -42,29 +31,15 @@ if [[ $? == 1 ]]; then
 fi
 rm temp.yml
 
-# Autofixing & Linting
-stylelint --fix "$CSS_PATH" &>/dev/null
-
 #───────────────────────────────────────────────────────────────────────────────
-
-# Update ToC
-printf "/* @TOC-SPLIT-MARKER */\n/*\n" >new_toc.css
-grep "< " "$CSS_PATH" | tr "<" "\t" |
-	sed -e "s/^# //" -e "s| \*/||" -e "s|/\* ||" -Ee "s/(\t*)/\1-/" >>new_toc.css
-split -p "@TOC-SPLIT-MARKER" "$CSS_PATH" temp
-mv tempaa before_toc.css
-mv tempac after_toc.css
-cat before_toc.css new_toc.css after_toc.css >"$CSS_PATH"
-rm new_toc.css before_toc.css tempab after_toc.css
 
 # Bump version number
 versionLine=$(grep -Ewn "^Version" "$CSS_PATH" | cut -d: -f1 | head -n1)
 currentVersion=$(sed -n "${versionLine}p" "$CSS_PATH" | cut -d. -f2)
 nextVersion=$((currentVersion + 1))
+manifest="$(dirname "$CSS_PATH")/manifest.json"
 sed -E -i '' "${versionLine}s/(.*\.)[[:digit:]]+/\1$nextVersion/" "$CSS_PATH"
-sed -E -i '' "s/(\"version\": \".*\.).*/\1$nextVersion\",/" "manifest.json"
-SECOND_MANIFEST="$(dirname "$CSS_PATH")/manifest.json"
-sed -E -i '' "s/(\"version\": \".*\.).*/\1$nextVersion\",/" "$SECOND_MANIFEST"
+sed -E -i '' "s/(\"version\": \".*\.).*/\1$nextVersion\",/" "$manifest"
 
 #───────────────────────────────────────────────────────────────────────────────
 
@@ -78,7 +53,7 @@ sed -E -i '' "s/badge.*-[[:digit:]]+-/badge\/downloads-$dl-/" ./README.md
 #───────────────────────────────────────────────────────────────────────────────
 
 # git add, commit, pull, and push
-# needs piping stderr to stdin, since git push reports an error even on success?!
+# needs piping stderr to stdin, since git push reports an error even on success
 git add --all && git commit -m "publish (automated)"
 git pull && git push 2>&1
 
