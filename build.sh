407439a82d30ea341fd928fb2a595f8dcd31957f
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-03-19 12:07:52 +0100

    build: fix symlink switch

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@
 # CONFIG
 
 script_dir="$(dirname "$(readlink -f "$0")")"
-CSS_PATH="$script_dir/theme.css"
+css_path="$script_dir/theme.css"
 
 #───────────────────────────────────────────────────────────────────────────────
 # TEST: YAML VALIDATION
@@ -23,11 +23,11 @@ npm_location="$(npm root)/.bin/"
 export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$npm_location:$PATH
 if ! command -v yaml-validator &>/dev/null; then echo "yaml-validator not installed." && return 1; fi
 
-sed -n '/@settings/,$p' "$CSS_PATH" | sed '1d;$d' | sed '$d' >temp.yml
-YAMLLINT_OUTPUT=$(yaml-validator temp.yml)
+sed -n '/@settings/,$p' "$css_path" | sed '1d;$d' | sed '$d' >temp.yml
+yamllint_output=$(yaml-validator temp.yml)
 if [[ $? == 1 ]]; then
 	echo "YAML ERROR"
-	echo "$YAMLLINT_OUTPUT" | sed '1d'
+	echo "$yamllint_output" | sed '1d'
 	return 1
 fi
 rm temp.yml
@@ -41,8 +41,8 @@ versionLnum=$(echo "$versionLine" | cut -d: -f1)
 currentVer=$(echo "$versionLine" | cut -d. -f2)
 nextVer=$((currentVer + 1))
 
-manifest="$(dirname "$CSS_PATH")/manifest.json"
-sed -E -i '' "${versionLnum} s/$currentVer/$nextVer/" "$CSS_PATH"
+manifest="$(dirname "$css_path")/manifest.json"
+sed -E -i '' "${versionLnum} s/$currentVer/$nextVer/" "$css_path"
 sed -E -i '' "/version/ s/$currentVer/$nextVer/" "$manifest"
 
 # Update Theme Download numbers in README.md
@@ -53,7 +53,7 @@ sed -E -i '' "s/badge.*-[[:digit:]]+-/badge\/downloads-$dl-/" ./README.md
 
 #───────────────────────────────────────────────────────────────────────────────
 # CHANGELOG
-commits_since_last_publish=$(git log :/publish.. --format="- %cs %s")
+commits_since_last_publish=$(git -C "$script_dir" log :/publish.. --format="- %cs %s")
 
 echo "$commits_since_last_publish" >>"temp.md"
 cat "Changelog.md" >>"temp.md"
@@ -73,17 +73,19 @@ git pull && git push 2>&1
 
 if [[ "$OSTYPE" =~ "darwin" ]]; then
 	# switch back to symlink
-	cd "$VAULT_PATH/.obsidian/themes/Shimmering Focus/" || return 1
-	cp "$CSS_PATH" "fallback.css"     # copy theme file for fallback
-	ln -sf "fallback.css" "theme.css" # re-create symlink
+	while read -r line; do
+		repo_path=$(echo "$line" | cut -d, -f2 | sed "s|^~|$HOME|")
+		theme_path="$repo_path/.obsidian/themes/Shimmering Focus"
+		[[ -d "$theme_path" ]] || continue
+		cd "$theme_path" || return 1
 
-	cd "$PHD_DATA_VAULT/.obsidian/themes/Shimmering Focus/" || return 1
-	cp "$CSS_PATH" "fallback.css"     
-	ln -sf "fallback.css" "theme.css" 
+		cp "$css_path" "fallback.css"     # copy theme file for fallback
+		ln -sf "fallback.css" "theme.css" # re-create symlink
+	done <"$HOME/.config/perma-repos.csv"
 
 	# confirmation sound
 	afplay "/System/Library/Components/CoreAudio.component/Contents/SharedSupport/SystemSounds/siri/jbl_confirm.caf" & # codespell-ignore
 
 	# delete this repo folder
-	rm -rf "$script_dir"              
+	rm -rf "$script_dir"
 fi
